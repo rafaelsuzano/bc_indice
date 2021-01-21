c151377186dd3ea9987d2059c78ed0222ecfe3e6
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-01-21 20:56:03 -0300

    ajuste

--- selic.py
+++ selic.py
@@ -1,14 +1,9 @@
-try: #python3
-    from urllib.request import urlopen
-except: #python2
-    from urllib2 import urlopen
-
-
-
+#import urllib.request
+from urllib.request import urlopen
 import requests
 import datetime
 import json 
-from urllib.request import urlopen
+
 #Listas
 vl =[]
 dvl =[]
@@ -16,11 +11,19 @@ dvl =[]
 #Calculo      
 def TotalIndice(V,VC):
     del V[0]
+  
     lista = [float(i) for i in V]
     TI= (sum(lista))
     I=((TI/100)+1)
     F =(I*VC)
     VF =(round(F,2))
+    
+    #for i, j in zip((dvl), (vl)):
+        
+     #   print("Data:"+str(i),"Valor:"+(j))
+      
+
+
     
     return VF
 
@@ -34,41 +37,34 @@ def ConvertStringToDate(d):
 def Captura_Selic(di,df,v):
 
     url='https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json&dataInicial='+di+'&dataFinal='+df
+    response = urlopen(url).read()
    
-
-    
     print(url)
-    response = requests.get(url)
-    #print(response.content)
-    data = (json.loads(response.content))
+    data = (json.loads(response))
+        
     
     for dados in data:
         dt = (dados['data'])
         vlr = str(dados['valor'])
-     
         
-        if ConvertStringToDate(di) >= ConvertStringToDate(dt)  :
+
+        if ConvertStringToDate(dt) <= ConvertStringToDate(di)  :
             pass
              
         else:  
-          
+            print (dados)    
             vl.append(vlr)
             dvl.append(dt) 
     
     VP =TotalIndice(vl,v)
-    #del dvl[0]
-    #del  vl[0]
+    
     
 
-    for i, j in zip((dvl), (vl)):
-        
-        print("Data:"+str(i),"Valor:"+(j))
-      
     
     print("Valor Inicial: R$ "+ str(v),"Data Inicio:"+ str(di), "Data Final:"+str(df), "Valor Corrigido: R$" + str(VP)  )
 
-di="01/01/2021"
-df="20/01/2021"
-v1 = 2000
+di="01/01/2020"
+df="31/01/2020"
+v1 = 200
 Captura_Selic(di,df,v1) 
   
