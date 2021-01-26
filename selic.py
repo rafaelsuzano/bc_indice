aadf6d4b095e4c9271999eb007cd81d1a6921910
Rafael Suzano <rafaelsuzano@hotmail.com>
2021-01-26 17:41:49 +0000

    Update selic.py

--- selic.py
+++ selic.py
@@ -3,29 +3,39 @@ from urllib.request import urlopen
 import requests
 import datetime
 import json 
+import pprint
+import numpy as np
+from functools import reduce
 
 #Listas
 vl =[]
 dvl =[]
 
 #Calculo      
-def TotalIndice(V,VC):
+def TotalIndice_Selic(V,VC):
     del V[0]
   
     lista = [float(i) for i in V]
     TI= (sum(lista))
-    I=((TI/100)+1)
+    I=(TI/100+1)
     F =(I*VC)
     VF =(round(F,2))
-    
-    #for i, j in zip((dvl), (vl)):
-        
-     #   print("Data:"+str(i),"Valor:"+(j))
-      
-
-
-    
     return VF
+    
+    
+def TotalIndice_IGPDI(V,VC):
+    ln=[]
+
+    lista = [float(i) for i in V]
+    for xx in lista:
+        x = (xx/100)+1
+        ln.append((x))
+   
+    I= (reduce(lambda x, y: x*y, ln))
+    VF =(I*VC)
+    VF =(round(VF,2))
+    return VF
+     
 
 #Convert string para data    
 def ConvertStringToDate(d):
@@ -41,7 +51,7 @@ def Captura_Selic(di,df,v):
    
     print(url)
     data = (json.loads(response))
-        
+    #print(data)    
     
     for dados in data:
         dt = (dados['data'])
@@ -52,19 +62,47 @@ def Captura_Selic(di,df,v):
             pass
              
         else:  
-            print (dados)    
+            
+           # print(dados)
+                    
             vl.append(vlr)
             dvl.append(dt) 
     
-    VP =TotalIndice(vl,v)
-    
-    
-
+    VP =TotalIndice_Selic(vl,v)
     
     print("Valor Inicial: R$ "+ str(v),"Data Inicio:"+ str(di), "Data Final:"+str(df), "Valor Corrigido: R$" + str(VP)  )
 
-di="01/01/2020"
-df="31/01/2020"
-v1 = 200
-Captura_Selic(di,df,v1) 
+
+ 
+
+def Captura_IGPDI(di,df,v):
+    
+    url='https://api.bcb.gov.br/dados/serie/bcdata.sgs.190/dados?formato=json&dataInicial='+di+'&dataFinal='+df
+    print(url)
+    response = urlopen(url).read()
+    print("Resultado da Correção pelo IGP-DI (FGV)")
+    data = (json.loads(response))
+    pprint.pprint(data)
   
+    
+    for dados in data:
+        dt = (dados['data'])
+        vlr = str(dados['valor'])
+                    
+        vl.append(vlr)
+        dvl.append(dt) 
+    
+    VP =TotalIndice_IGPDI(vl,v)
+ 
+    print("Valor Inicial: R$ "+ str(v),"Data Inicio:"+ str(di), "Data Final:"+str(df), "Valor Corrigido: R$" + str(VP)  )
+
+
+  
+  
+  
+di="01/01/2020"
+df="31/12/2020"
+v1 = 200
+#Captura_Selic(di,df,v1) 
+Captura_IGPDI(di,df,v1) 
+
